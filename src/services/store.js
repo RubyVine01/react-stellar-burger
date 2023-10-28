01af8fc0f907836a267612c7b58af523636407c1
RubyVine01 <leraprokofevaiera1@gmail.com>
2023-10-29 00:44:31 +0300

    add store

---
+++ src/services/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+//import reducer from "./payload.js";
+
+export const store = configureStore({
+  reducer: {},
+});
+
+//reducer: { key: reducer },
