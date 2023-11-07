b649ff01a8db7fc51f9ffd39267a19a55444e329
RubyVine01 <leraprokofevaiera1@gmail.com>
2023-11-07 18:57:39 +0200

    .

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -11,11 +11,8 @@ import BurgerConstructor from "../burger-constructor/burger-constructor.jsx";
 
 import { fetchIngredients } from "../../services/thunks/ingredients-data-thunk";
 import LoginPage from "../../pages/login-page/login-page";
-import RegistrationPage from "../../pages/registration-page/registration-page";
-import ForgotPasswordPage from "../../pages/forgot-password-page/forgot-password-page";
-import NewPasswordPage from "../../pages/new-password-page/new-password-page";
-import UserProfilPage from "../../pages/user-profil-page/user-profil-page";
 import IngredientPage from "../../pages/ingredient-page/ingredient-page";
+import UserProfilPage from "../../pages/user-profil-page/user-profil-page";
 
 function App() {
   const dispatch = useDispatch();
